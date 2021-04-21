4cf05eb5008f6feb5e57dbbaef03a0098542d505
Tam Ka Cheuk <u3547504@connect.hku.hk>
2021-04-21 22:01:35 +0800

    Trial of Case model

--- server/trial_delete_later/models.py
+++ server/trial_delete_later/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Case(models.Model):
-    name = models.CharField(max_length=50)
-    hasCovid = models.BooleanField(default=True)
+    case_number = models.UUIDField(
+                primary_key=True,
+                default=uuid.uuid4,
+                editable=False)
+    person_name = models.CharField(max_length=50)
+    identify_document_number = models.CharField(max_length=50, unique=True)
+    date_of_birth = models.DateField()
+    date_of_onset_of_symptoms = models.DateField()
+    date_of_confirmation_of_infection_by_testing = models.DateField()
 
     def _str_(self):
-        return self.name
+        return self.case_number
