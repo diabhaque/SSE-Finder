8d0b3e18ef4701adb3e8f05b2291270325776389
Diab Haque <diabhaque@gmail.com>
2021-04-29 23:39:54 +0800

    UI: Navigation Setup done. No login.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,54 @@
-import './App.css';
-import { Home } from './home/Home';
+import "./App.css";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
+import { Home } from "./home/Home";
+
+// Move components to separate page.
+
+const CaseRecordsPage = () => {
+    return <div>Case Records</div>;
+};
+
+const FindSsePage = () => {
+    return <div>Find SSEs</div>;
+};
+const AddCasePage = () => {
+    return <div>Add Case</div>;
+};
+
+const SideBarLayout = () => {
+    return (
+        <Home>
+            <Switch>
+                <Route exact path="/">
+                    <CaseRecordsPage />
+                </Route>
+                <Route exact path="/find-sse">
+                    <FindSsePage />
+                </Route>
+                <Route exact path="/add-case">
+                    <AddCasePage />
+                </Route>
+            </Switch>
+        </Home>
+    );
+};
 
 function App() {
-  return (
-    <Home/>
-  );
+    return (
+        <div className="App" style={{ height: "100vh" }}>
+            <Router>
+                <Switch>
+                    <Route path="/">
+                        <SideBarLayout />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
--- web/src/home/Home.tsx
+++ web/src/home/Home.tsx
@@ -1,25 +1,65 @@
-import { useEffect, useState } from "react";
-import { getCases } from "../client/requests";
-import { Case } from "../types/caseTypes_trial";
+import { Layout, Menu, Typography } from "antd";
+import { Link, useLocation } from "react-router-dom";
+import "./home.css";
 
-export const Home = () => {
-    const [cases, setCases] = useState<Case[]>([]);
+const { Header, Content, Sider } = Layout;
+const { Title } = Typography;
 
-    useEffect(() => {
-        let isCancelled = false;
+const MyMenu = Menu as any; // Ugly, but will try fixing later. Maybe.
+const MyLayout = Layout as any;
+interface HomeProps {
+    children: React.ReactNode;
+}
 
-        getCases().then((r) => {
-            if (!isCancelled && r) {
-                setCases(r);
-            }
-        });
+export const Home: React.FC<HomeProps> = ({ children }) => {
+    const location = useLocation();
+    const activeNav = location.pathname.split("/")[1] || "case-records";
 
-        return () => {
-            isCancelled = true;
-        };
-    }, []);
+    return (
+        <MyLayout style={{ height: "100vh" }}>
+            <Sider>
+                <MyMenu
+                    mode="inline"
+                    theme="dark"
+                    selectedKeys={[activeNav]}
+                >
+                    <div
+                        style={{
+                            textAlign: "center",
+                            margin: "16px",
+                            height: "32px"
+                        }}
+                    >
+                        <Title level={3} style={{ color: "white" }}>
+                            SSE Finder
+                        </Title>
+                    </div>
 
-    return <ul>
-        {cases.map(x => <li key={x.name}>{x.name} {x.hasCovid? 'has Covid' : 'does not have Covid'}</li>)}
-    </ul>;
+                    <MyMenu.Item key="case-records">
+                        <Link to="/">Case Records</Link>
+                    </MyMenu.Item>
+                    <MyMenu.Item key="find-sse">
+                        <Link to="/find-sse">Find SSEs</Link>
+                    </MyMenu.Item>
+                    <MyMenu.Item key="add-case">
+                        <Link to="/add-case">Add Case</Link>
+                    </MyMenu.Item>
+                </MyMenu>
+            </Sider>
+            <MyLayout>
+                <Header
+                    className="site-layout-sub-header-background"
+                    style={{ padding: 0 }}
+                />
+                <Content style={{ margin: "24px 16px 0" }}>
+                    <div
+                        className="site-layout-background"
+                        style={{ padding: 24, minHeight: 360 }}
+                    >
+                        {children}
+                    </div>
+                </Content>
+            </MyLayout>
+        </MyLayout>
+    );
 };
