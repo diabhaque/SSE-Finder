b9c67be2729537efbdb02a8041396476ea25bff7
Diab Haque <diabhaque@gmail.com>
2021-05-02 20:49:02 +0800

    Missed URLs

--- server/server_config/urls.py
+++ server/server_config/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('trial_delete_later.urls'))
+    path('api/', include('trial_delete_later.urls')),
+    path('', index)
 ]
